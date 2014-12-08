0fd554c5d67ae470c02d750cdfb3e87961eb8a03
Abdul Jaleel <ajaleelp@gmail.com>
2014-12-08 16:58:28 +0530

    End of chp 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def title_correction(page_title='')
+		base_title = "Squadra Treasure Hunt '15"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end

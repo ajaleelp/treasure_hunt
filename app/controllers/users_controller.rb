b35322819efa73b24a84dfc89d2d711fd97b639a
Abdul Jaleel <ajaleelp@gmail.com>
2014-12-09 22:48:57 +0530

    End of chp 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		flash[:success]= "Welcome onboard Pirate #{@user.name.split.first}"
+      log_in @user
+  		flash[:success]= "Welcome onboard Pirate #{@user.name.split.first}!"
   		redirect_to @user
   	else
   		render 'new'
